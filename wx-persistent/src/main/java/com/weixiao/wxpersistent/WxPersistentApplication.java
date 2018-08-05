3690f4b8ef9af6142b6f339ae90f9043b1f573ed
weixiaoxuexi <xunvvv@126.com>
2018-08-05 19:43:56 +0800

    初始化工程

---
+++ wx-persistent/src/main/java/com/weixiao/wxpersistent/WxPersistentApplication.java
@@ -0,0 +1,12 @@
+package com.weixiao.wxpersistent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxPersistentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxPersistentApplication.class, args);
+	}
+}
---
+++ wx-web/src/main/java/com/weixiao/wxweb/WxWebApplication.java
@@ -0,0 +1,12 @@
+package com.weixiao.wxweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxWebApplication.class, args);
+	}
+}
